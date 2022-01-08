4a50e564837b521a833e2562389ee32713028f6a
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-08 19:12:23 +0700

    set up flexbox and catch error for background and author name

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const apiKey = `NEdkcl7_x9O2DlOiH5BU2eOW6QjGgVABkeLGQi-Jzgg`;
-let headers = {
+const author = document.querySelector('.author');
+
+const headers = {
   Authorization: `Client-ID NEdkcl7_x9O2DlOiH5BU2eOW6QjGgVABkeLGQi-Jzgg`,
 };
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=NEdkcl7_x9O2DlOiH5BU2eOW6QjGgVABkeLGQi-Jzgg`;
-console.log(apiUrl);
+const apiUrl = `https://api.unsplash.com/photos/random/?orientation=landscape&query=nature`;
 
-fetch(
-  'https://api.unsplash.com/photos/random/?orientation=landscape&query=nature',
-  {
-    headers: headers,
-  }
-)
+fetch(apiUrl, {
+  headers: headers,
+})
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    author.textContent = `By: ${data.user.name}`;
+  })
+  .catch((err) => {
+    document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1462400362591-9ca55235346a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyODkzNjR8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDE2NDM4Njk&ixlib=rb-1.2.1&q=80&w=1080')`;
+    author.textContent = `By: Christian Joudrey`;
   });
